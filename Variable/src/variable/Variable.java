e10c0104c982902bbc47f0d94c3064173a9fb1d3
YenNu1402 <yennu1422005@gmail.com>
2024-08-24 18:13:27 +0700

    init project

---
+++ Variable/src/variable/Variable.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variable;
+
+/**
+ *
+ * @author lethiyennu
+ */
+public class Variable {
+
+    public static void main(String[] args) {
+        int x=5;
+        System.out.println("x"+x);
+        System.out.println(x+x);
+    }
+    
+}
---
+++ Variable/src/variable/Variable3.java
@@ -0,0 +1,8 @@
+package variable;
+public class Variable3 {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+       // PI = PI + 1; => sai vi PI la hang so
+        System.out.println("pi= " + PI);
+    }
+}
